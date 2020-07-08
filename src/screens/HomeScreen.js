8b03791becc60835100c56f68f986c300eb6a477
angelicrf <angelicrf@gmail.com>
2020-07-08 13:59:45 -0700

    change 1

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,14 +2,13 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Angelique Expo Screen</Text>;
+  return <Text style={styles.text}>Angelique Expo Screen Initial</Text>;
 };
 
 const styles = StyleSheet.create({
   text: {
         fontSize: 30,
-        backgroundColor: "blue",
-       
+        backgroundColor: "blue",       
   }
 });
 
