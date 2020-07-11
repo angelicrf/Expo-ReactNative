cfc41848bfe9eb3645e924486ff57cf1480d8e5d
angelicrf <angelicrf@gmail.com>
2020-07-11 14:23:22 -0700

    error handler

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import ColorComponent from "../Components/ColorComponent";
 
+const COLOR_INCREMENT = 1;
+
 const ColorScreen = () => {
   const [red, setRed] = useState(0);
   const [blue, setBlue] = useState(0);
@@ -11,22 +13,41 @@ const ColorScreen = () => {
   console.log(blue);
   console.log(green);
 
+  const setColor = (color, change) => {
+    switch (color) {
+      case "red":
+        red + change > 255 || red - change < 0 ? null : setRed(red + change);
+        return;
+      case "blue":
+        blue + change > 255 || blue - change < 0
+          ? null
+          : setBlue(blue + change);
+        return;
+      case "green":
+        green + change > 255 || green - change < 0
+          ? null
+          : setGreen(green + change);
+        return;
+      default:
+        return;
+    }
+  };
   return (
     <View style={styles.viewStyle}>
       <ColorComponent
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setColor("red", COLOR_INCREMENT)}
+        onDecrease={() => setColor("red", -1 * COLOR_INCREMENT)}
         color="Red"
       />
       <ColorComponent
         color="Blue"
-        onIncrease={() => setBlue(blue + 1)}
-        onDecrease={() => setBlue(blue - 1)}
+        onIncrease={() => setColor("blue", COLOR_INCREMENT)}
+        onDecrease={() => setColor("blue", -1 * COLOR_INCREMENT)}
       />
       <ColorComponent
         color="Green"
-        onIncrease={() => setGreen(green + 1)}
-        onDecrease={() => setGreen(green - 1)}
+        onIncrease={() => setColor("green", COLOR_INCREMENT)}
+        onDecrease={() => setColor("green", -1 * COLOR_INCREMENT)}
       />
       <View
         style={{
