11497d8ffb8b8b3ca8b7c264495524d5ea26529d
angelicrf <angelicrf@gmail.com>
2020-07-13 13:51:07 -0700

    search bar

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,18 +1,41 @@
-import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import React, { useState } from "react";
+import { Text, StyleSheet, TextInput, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
-const Search = ({ name }) => {
+const Search = ({ term, onNameChange, OnTermSubmit }) => {
   return (
-    <View style={styles.viewCompon}>
-      <Text>Search Component </Text>
-
-      <Text>Searched name is: {name}</Text>
+    <View style={styles.viewStylet}>
+      <Feather style={styles.iconStyle} name="search" />
+      <TextInput
+        style={styles.textInput}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={term}
+        onChangeText={(someName) => onNameChange(someName)}
+        onEndEditing={() => OnTermSubmit()}
+      >
+        Searching
+      </TextInput>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  viewCompon: {
-    margin: 30,
+  viewStylet: {
+    marginHorizontal: 15,
+    marginTop: 15,
+    backgroundColor: "#F0EEEE",
+    borderRadius: 5,
+    height: 50,
+    flexDirection: "row",
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 10,
+  },
+  textInput: {
+    flex: 1,
+    fontSize: 18,
   },
 });
 export default Search;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,28 +1,25 @@
 import React, { useState } from "react";
-import { Text, StyleSheet, TextInput, View } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 import Search from "../Components/Search";
 
-const ComponentScreen = () => {
-  const [name, setName] = useState("");
+const SearchScreen = () => {
+  const [term, setTerm] = useState("");
 
   return (
-    <View>
-      <TextInput
-        style={styles.textInput}
-        autoCapitalize="none"
-        autoCorrect={false}
-        value={name}
-        onChangeText={(someText) => setName(someText)}
+    <View style={styles.viewCompon}>
+      <Text>Search Component </Text>
+      <Search
+        name={term}
+        onNameChange={(textChange) => setTerm(textChange)}
+        OnTermSubmit={() => console.log("Search submitted...")}
       />
-      <Search name={name} />
+      <Text>{term}</Text>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  textInput: {
-    margin: 10,
-    borderColor: "black",
-    borderWidth: 1,
+  viewCompon: {
+    margin: 30,
   },
 });
-export default ComponentScreen;
+export default SearchScreen;
