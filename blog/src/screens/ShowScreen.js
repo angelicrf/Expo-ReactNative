cd4a046fb37b05e9c7eb35ec0080f728588e7dd5
angelicrf <angelicrf@gmail.com>
2020-07-17 10:24:11 -0700

    Show Screen created

---
+++ blog/src/screens/ShowScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ShowScreen = () => {
+  return (
+    <View>
+      <Text>From show screen</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
+export default ShowScreen;
