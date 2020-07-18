363a1120f7d26ec329242d12e9cb3c649891b96b
angelicrf <angelicrf@gmail.com>
2020-07-18 12:20:10 -0700

    8 hours limit of url server -http://425a5dec7fe1.ngrok.io

--- blog/src/components/BlogPostForm.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-
-const BlogPostForm = () => {
-  return <Text>From Blog Post</Text>;
-};
-const styles = StyleSheet.create({});
-export default BlogPostForm;
