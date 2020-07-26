057c36646c21ed2df3a31ece178a87f580b419e2
angelicrf <angelicrf@gmail.com>
2020-07-25 18:01:46 -0700

    sign in - sign out -userAuth

--- track-server/routes/authRoutes.js
+++ track-server/routes/authRoutes.js
@@ -10,9 +10,30 @@ router.post("/signup", async (req, res) => {
     const user = new User({ email, password });
     await user.save();
     const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    console.log(token);
     res.send({ token });
   } catch (e) {
     return res.status(422).send(err);
+  }
+});
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ err: "There is an error from email or password" });
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).send({ error: "Email not found" });
+  }
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    console.log(token);
+    res.send({ token });
+  } catch (e) {
+    res.status(401).send({ error: "Invalid email or password" });
   }
 });
 module.exports = router;
