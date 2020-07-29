b1b3b7e95fff383b735bfb9fb64556e0f18a01dc
angelicrf <angelicrf@gmail.com>
2020-07-29 09:17:41 -0700

    navigator - navigate

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://9e35a08f302f.ngrok.io"
+  baseURL: "http://1d265ea4ae7c.ngrok.io"
 });
--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,26 +1,33 @@
 import createDataContext from "./createDataContext";
 import tracker from "../api/tracker";
+import { AsyncStorage } from "react-native";
+import { navigate } from "../navigationRef";
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case "Add_Error":
       return { ...state, errorMessage: action.payload };
+    case "signup":
+      return { errorMessage: "", token: action.payload };
+
     default:
       return state;
   }
 };
-const signup = dispatch => {
-  return async (email, password) => {
-    try {
-      const response = await tracker.post("/signup", { email, password });
-      console.log(response.data);
-    } catch (e) {
-      dispatch({
-        type: "Add_Error",
-        payload: "Something is wrong with signup"
-      });
-    }
-  };
+const signup = dispatch => async (email, password) => {
+  try {
+    const response = await tracker.post("/signup", { email, password });
+    console.log(response.data);
+    await AsyncStorage.setItem(response.data.token);
+    //await AsyncStorage.getItem("token");
+    dispatch({ type: "signup", payload: response.data.token });
+    navigate("mainFlow");
+  } catch (e) {
+    dispatch({
+      type: "Add_Error",
+      payload: "Something is wrong with signup"
+    });
+  }
 };
 const signin = dispatch => {
   return () => {};
@@ -31,5 +38,5 @@ const signout = dispatch => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { isSignedIn: false, errorMessage: "" }
+  { token: null, errorMessage: "" }
 );
